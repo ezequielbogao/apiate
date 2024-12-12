6031a7d3c35d8493ced9d14cebcc3be5d32d92ca
Ezequiel <ezequiel.bogao@msm.gov.ar>
2024-12-12 15:36:07 -0300

    Agregado de limites para mapa san miguel

--- src/layouts/components/MapReclamos.jsx
+++ src/layouts/components/MapReclamos.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useRef, useState } from "react";
-import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import { MapContainer, TileLayer, Marker, Popup, Polygon } from "react-leaflet";
 // import L from "leaflet";
 import "leaflet/dist/leaflet.css";
 import { useMenu } from "../../Context/MenuContext";
@@ -42,6 +42,30 @@ const MapReclamos = () => {
             }
         }
     }, [persona]);
+
+    const polygon = [
+        [-34.502726, -58.644883],
+        [-34.534328, -58.606439],
+        [-34.605472, -58.713749],
+        [-34.557904, -58.7769],
+        [-34.549944, -58.768073],
+        [-34.551413, -58.766168],
+        [-34.546772, -58.758194],
+        [-34.550835, -58.756388],
+        [-34.548432, -58.753835],
+        [-34.548829, -58.751567],
+        [-34.52589, -58.728661],
+        [-34.525773, -58.728833],
+        [-34.521639, -58.723032],
+        [-34.520639, -58.723519],
+        [-34.517704, -58.718775],
+        [-34.532677, -58.700953],
+        [-34.525349, -58.693572],
+        [-34.52975, -58.686992],
+    ];
+
+    const purpleOptions = { color: "purple" };
+
     return (
         persona && (
             <div style={{ height: "100vh", width: "100%" }}>
@@ -64,6 +88,7 @@ const MapReclamos = () => {
                             </Popup>
                         </Marker>
                     ))}
+                    <Polygon pathOptions={purpleOptions} positions={polygon} />
                 </MapContainer>
             </div>
         )
